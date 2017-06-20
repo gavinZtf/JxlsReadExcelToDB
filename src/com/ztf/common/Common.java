3f86c9cbed192f5892e80f8f23361b4fd4e2bf7f
gavin <2605170374@qq.com>
2017-06-20 23:14:56 +0800

    java 导入数据到excel

---
+++ src/com/ztf/common/Common.java
@@ -0,0 +1,16 @@
+package com.ztf.common;
+
+/**
+ * @author Gavin
+ */
+public class Common {
+
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_NAME = "excelToDB";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root123";
+    public static final String IP = "127.0.0.1";
+    public static final String PORT = "3306";
+    public static final String URL = "jdbc:mysql://" + IP + ":" + PORT + "/" + DB_NAME;
+    
+}
--- src/com/ztf/test/Main.java
+++ src/com/ztf/test/Main.java
@@ -1,8 +1,10 @@
 package com.ztf.test;
 
 import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,13 +14,16 @@ import org.jxls.reader.ReaderBuilder;
 import org.jxls.reader.XLSReadStatus;
 import org.jxls.reader.XLSReader;
 
+import com.ztf.common.Common;
 import com.ztf.entity.Department;
+import com.ztf.entity.Employee;
 
 public class Main {
 	
 	private static String dataFile = "departmentdata.xls";
     public static final String xmlConfig = "xmlConfig.xml";
 
+	@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
 	public static void main(String[] args) {
 		try {
 			InputStream inputXML = new BufferedInputStream(Main.class.getResourceAsStream(xmlConfig));
@@ -33,9 +38,47 @@ public class Main {
 			beans.put("departments", departments);
 		    XLSReadStatus readStatus = mainReader.read(inputXLS, beans);
 		    System.out.println(department.getName());
+		    
+		    
+		    //get the data object
+		    List<Employee> empList = department.getStaff();
+		    
+		    String sql = "insert into employee (name, age, payment, bonus) values (?, ?, ?, ?)";
+		    Connection connection = getConnection();
+		    connection.setAutoCommit(false);
+		    PreparedStatement ps = connection.prepareStatement(sql);
+		    final int batchSize = 1000;
+		    int count = 0;
+		    for (Employee employee: empList) {
+		        ps.setString(1, employee.getName());
+		        ps.setInt(2, employee.getAge());
+		        ps.setDouble(3, employee.getPayment());
+		        ps.setDouble(4, employee.getBonus());
+		        ps.addBatch();
+//		        if(++count % batchSize == 0) {
+//		            ps.executeBatch();
+//		        }
+		    }
+		    ps.executeBatch(); // insert remaining records
+		    connection.commit();
+		    ps.close();
+		    connection.close();
+		    
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static Connection getConnection() {
+		try {
+			Class.forName(Common.DRIVER);
+			Connection conn = DriverManager.getConnection(Common.URL, Common.USERNAME, Common.PASSWORD);
+			return conn;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null; 
 	}
 
 }
